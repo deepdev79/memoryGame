ba132552dbaf31300a314364f47e5d864023cf00
Deepankar <deepankar7493@gmail.com>
2025-12-17 15:14:53 +0530

    game logic working

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,16 @@
-import { use, useEffect, useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
 
 import "./App.css";
 
-const test = [
-  {
-    id: 1,
-    img: "/1.jpg",
-  },
-  {
-    id: 2,
-    img: "/2.jpg",
-  },
-  {
-    id: 3,
-    img: "/3.jpg",
-  },
-  {
-    id: 4,
-    img: "/4.jpg",
-  },
-  {
-    id: 5,
-    img: "/5.jpg",
-  },
-  {
-    id: 6,
-    img: "/6.jpg",
-  },
-];
 interface FetchPokemon {
   id: number;
   monPic: string;
 }
 function App() {
   const [pokemons, setPokemons] = useState<FetchPokemon[]>([]);
+  const [score, setScore] = useState(0);
+  const [prevClick, setPrevClick] = useState<number[]>([]);
+  const bestScore = useRef(0);
 
   useEffect(function () {
     async function getPokemon(): Promise<void> {
@@ -54,22 +31,50 @@ function App() {
     }
     getPokemon();
   }, []);
+
+  function handlePokemonClick(id: number) {
+    if (prevClick.length === 0) {
+      setPrevClick((prev) => [...prev, id]);
+      setScore(1);
+      return;
+    } else if (prevClick.includes(id)) {
+      if (score > bestScore.current) bestScore.current = score;
+      setScore(0);
+      setPrevClick([]);
+      return;
+    }
+    setPrevClick((prev) => [...prev, id]);
+    setScore((s) => s + 1);
+  }
+
+  function shufflePokemons() {
+    setPokemons((prevPokemons) => {
+      const shuffled = [...prevPokemons];
+      for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+      }
+      return shuffled;
+    });
+  }
+
   return (
     <div className="container">
       <h1>Memory Game</h1>
-      <p>Score:</p>
-      <p>Best score:</p>
-      <Pokemons mons={pokemons} />
+      <p>Score: {score}</p>
+      <p>Best score: {bestScore.current}</p>
+      <Pokemons mons={pokemons} onPokemonClick={handlePokemonClick} />
+      <button onClick={shufflePokemons}>Click me</button>
     </div>
   );
 }
 
-function Pokemons({ mons }) {
+function Pokemons({ mons, onPokemonClick }) {
   return (
     <div className="mons">
       {mons.map((el) => (
-        <div className="box">
-          <img src={el.monPic} />
+        <div className="box" key={el.id} onClick={() => onPokemonClick(el.id)}>
+          <img src={el.monPic} alt={`Pokemon ${el.id}`} />
         </div>
       ))}
     </div>
